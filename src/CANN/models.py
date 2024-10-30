cleaned up code, created bounds for variance, and fixed Is_max bug

src/CANN/models.py
@@ -187,8 +187,7 @@ def ortho_cann_3ff(lam_ut_all, gamma_ss, P_ut_all, P_ss, modelFit_mode, alpha, s
     Is_max = get_max_inv_mesh(reshape_input_output_mesh(lam_ut_all), modelFit_mode)
     P_ut_reshaped = np.array(reshape_input_output_mesh(P_ut_all))
     lam_ut_reshaped = np.array(reshape_input_output_mesh(lam_ut_all))
-    scale_factors = np.mean(P_ut_reshaped, axis=-1) * (np.max(lam_ut_reshaped, axis=-1) - np.min(lam_ut_reshaped, axis=-1)) # * 10 because there are 5 loading configurations and 2
-    scale_factor = np.sum(scale_factors)
+
 
     # Inputs defined
     I1_in = tf.keras.Input(shape=(1,), name='I1')
@@ -222,8 +221,12 @@ def ortho_cann_3ff(lam_ut_all, gamma_ss, P_ut_all, P_ss, modelFit_mode, alpha, s
     if two_term:
         ALL_I_out = tf.keras.layers.concatenate([ALL_I_out[:, term:(term+1)] for term in terms], axis=1)
     terms = ALL_I_out.get_shape().as_list()[1]
-    # ALL_I_out_scaled = keras.layers.Lambda(lambda x: (x / terms * 2 * scale_factor))(ALL_I_out)
-    Psi_model = keras.models.Model(inputs=[I1_in, I2_in, I4f_in, I4n_in, I8fn_in], outputs=[ALL_I_out], name='Psi')
+
+    scale_factors = np.mean(P_ut_reshaped, axis=-1) * (np.max(lam_ut_reshaped, axis=-1) - np.min(lam_ut_reshaped,
+                                                                                                 axis=-1))  # * 10 because there are 5 loading configurations and 2
+    scale_factor = np.sum(scale_factors) / terms * 2
+    ALL_I_out_scaled = keras.layers.Lambda(lambda x: (x * scale_factor))(ALL_I_out)
+    Psi_model = keras.models.Model(inputs=[I1_in, I2_in, I4f_in, I4n_in, I8fn_in], outputs=[ALL_I_out_scaled], name='Psi')
 
     return Psi_model, terms  # 32 terms
 

src/main.py
@@ -1,35 +1,17 @@
-from synth_data import *
-from models import *
-from bcann import train_bcanns
-import tensorflow as tf
+from src.bcann_vae import disp_equation_weights_bcann, train_bcanns
+from src.plotting import plot_bcann
+from src.utils import *
+
+
 if __name__ == "__main__":
 
     stretches, stresses = load_data()
-    # params_train, stretches_train, stresses_train = generate_data(500)
-    # params, stretches_test, stresses_test = generate_data(10)
+    model_given, lam_ut_all, P_ut_all = train_bcanns(stretches[0, :, :], stresses, should_train=True) # Test if works the same as before
 
-    # train_bcanns()
-
-    # # for i in range(17):
-    # #     print(i)
-    # # params = np.random.rand(34)
-    # # test_cann(stretches_train, params)
-    # tf.keras.backend.set_floa
-    # tx('float64')
-    train_bcanns(stretches[0, :, :], stresses, should_train=False, kevins_version=False) # Test if works the same as before
+    modelFit_mode = "0123456789"
+    # Print model equation and weights (with variances)
+    disp_equation_weights_bcann(model_given.get_weights(), lam_ut_all, P_ut_all, modelFit_mode)
 
 
-    # validate_cann(stretches_test[0, :, :], stresses_test[0, :, :], params[0, :])
-    #
-    # tf.keras.backend.set_floatx('float64')
-    #
-    #
-    # model = train_vae(stretches_train, stresses_train)
-    # model = load_vae(stretches_train, stresses_train)
-    # # #
-    # # test_vae(model, stretches_test, stresses_test)
-    #
-    # stress_map = np.zeros_like(stresses_test[0, :, :])
-    # stress_map[0:500, :] = 1
-
-    # nuts_sample(model, stretches_test, stress_map, stresses_test[0, :, :])
+    plot_bcann(stretches, stresses, model_given, lam_ut_all, False)
+    plot_bcann(stretches, stresses, model_given, lam_ut_all, True)

src/obsolete/synth_data.py
@@ -1,5 +1,8 @@
 import numpy as np
-from utils import *
+from src.utils import *
+
+from src.utils import get_output_grads
+
 
 # def generate_data(num_samples):
 #     log_means = np.array([9.598, 2.824, 2.387, 3.415, 2.752, 1.498, 1.553, 3.207, 1.866, 1.762])

src/plotting.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 from src.CANN.cont_mech import *
 from src.CANN.models import *
 import matplotlib
+from matplotlib.patches import Patch
+from matplotlib.ticker import AutoMinorLocator
+
 
 ## Override default matplotlib setting so plots look better
 # plt.rcParams["font.family"] = "Helvetica"
@@ -33,15 +36,36 @@ plt.rcParams["figure.titleweight"] = "extra bold"
 matplotlib.rcParams['legend.handlelength'] = 1
 matplotlib.rcParams['legend.handleheight'] = 1
 # matplotlib.rcParams['legend.fontsize'] = 60
-def plot_bcann(stretch, stress_true, stress_pred, stress_std_pred, kevins_version):
-    # already reshaped
-    # stretch: 5 x 100 x 2
-    # stress_true: 2 x 5(n_ex) x 5 x 100 x 2( x/y)
-    # stress_pred: 2 x 5 x 100 x 2( x/y)
 
+
+def plot_bcann(stretches, stresses, model_given, lam_ut_all, terms):
+
+    #### Get model Predictions ####
+    # Means and variances
+    Stress_predict_UT = model_given.predict(lam_ut_all).reshape((-1, 2, 2, 2))  # N x 8 (45, xy, mean/var)
+
+    stretch_plot = stretches.reshape((2, 5, 5, -1, 2))[0, 0, :, :, :]  # 5 x 100 x 2
+    stress_true = stresses.reshape((2, 5, 5, -1, 2)) # 2 x 5(n_ex) x 5 x 100 x 2( x/y)
+
+    stress_pred_mean = np.array(Stress_predict_UT[:, :, :, 0]).transpose((1, 0, 2)).reshape((2, 5, 5, -1, 2))[:, 0, :, :,
+                      :]  # 2 x 5 x 100 x 2( x/y)
+    stress_pred_std = np.sqrt(
+        np.array(Stress_predict_UT[:, :, :, 1]).transpose((1, 0, 2)).reshape((2, 5, 5, -1, 2)))[:, 0, :, :,
+                          :]  # 2 x 5 x 100 x 2( x/y)
+
+    # Predictions by term
+    model_weights = model_given.get_weights()
+    n_terms = len(model_weights) // 3
+    stress_pred_terms = np.zeros(shape = stress_pred_mean.shape + (n_terms + 1,))
+    for i in range(n_terms):
+        temp_weights = [model_weights[j] * (i == j // 3) for j in range(n_terms * 3)]
+        model_given.set_weights(temp_weights)
+        temp_preds = model_given.predict(lam_ut_all).reshape((-1, 2, 2, 2))
+        stress_pred_terms[:, :, :, :, i + 1] = stress_pred_terms[:, :, :, :, i] + np.array(temp_preds[:, :, :, 0]).transpose((1, 0, 2)).reshape((2, 5, 5, -1, 2))[:, 0, :, :, :]
+    model_given.set_weights(model_weights)
     # Plot Best Fit
     plt.rcParams['text.usetex'] = False
-    plt.rcParams['figure.figsize'] = [30, 15]
+    plt.rcParams['figure.figsize'] = [30, 20 if terms else 15]
     plt.rcParams['figure.constrained_layout.use'] = True
 
 
@@ -51,22 +75,32 @@ def plot_bcann(stretch, stress_true, stress_pred, stress_std_pred, kevins_versio
     direction_strings = ["w", "s"] * 5 + ["x", "y", "x", "y", "x"]
     n_spaces = 55
     titles = ["strip-w", None, "off-w", None, "equibiax-ws", None, "off-s", None, "strip-s", None, " " * n_spaces + "strip-x", None," " * n_spaces + "off-x", None, "equibiax-xy"]
-    inputs = [[stretch[j, :, k] for k in range(2)] for i in range(2) for j in range(5) ]
+    inputs = [[stretch_plot[j, :, k] for k in range(2)] for i in range(2) for j in range(5) ]
     inputs = [[x[i] if np.max(x[i]) > 1.0 else ((x[1 - i] - 1) * 1e-9 + 1) for i in range(2)] for x in inputs]
     inputs = flatten(inputs)[0:15] # 100 len each
     outputs = [stress_true[i, :, j, :, k] for i in range(2) for j in range(5) for k in range(2)][0:15] # 5 x 100 each
-    pred_mean = [stress_pred[i, j, :, k] for i in range(2) for j in range(5) for k in range(2)][0:15]
-    pred_std = [stress_std_pred[i, j, :, k] for i in range(2) for j in range(5) for k in range(2)][0:15]
-    # print(pred_mean)
-    # print(pred_std)
+    pred_mean = [stress_pred_mean[i, j, :, k] for i in range(2) for j in range(5) for k in range(2)][0:15]
+    pred_std = [stress_pred_std[i, j, :, k] for i in range(2) for j in range(5) for k in range(2)][0:15]
+    pred_terms = [stress_pred_terms[i, j, :, k, :] for i in range(2) for j in range(5) for k in range(2)][0:15]
+    cmap = plt.cm.get_cmap('jet_r', 17)  # define the colormap
+    cmaplist = [cmap(i) for i in range(cmap.N)]
 
     num_points = 17
     for i in range(len(axes)):
-        # Create plot that fills between the lower and upper bound
-        axes[i].fill_between(inputs[i], pred_mean[i] - pred_std[i], pred_mean[i] + pred_std[i], lw=0, zorder=i + 1, color="#C0C7EC",
-                         label=i + 1)
+        if terms:
+            for j in range(n_terms):
+                # Create plot that fills between the lower and upper bound
+                axes[i].fill_between(inputs[i], pred_terms[i][:, j], pred_terms[i][:, j + 1], lw=0,
+                                     zorder=j + 1, color=cmaplist[j],
+                                     label=j + 1)
+                axes[i].plot(inputs[i], pred_terms[i][:, j + 1],  lw=0.4, zorder=23, color='k')
+        else:
+            # Create plot that fills between the lower and upper bound
+            axes[i].fill_between(inputs[i], pred_mean[i] - pred_std[i], pred_mean[i] + pred_std[i], lw=0, zorder=i + 1, color="#C0C7EC",
+                             label=i + 1)
+
         # Create plot that draws a line on the upper bound
-        axes[i].plot(inputs[i], pred_mean[i], lw=4, zorder=24, color='k')
+        scatterplot = axes[i].plot(inputs[i], pred_mean[i], lw=4, zorder=24, color='k')
 
         min_P = np.min(outputs[i])
         max_P = np.max(outputs[i])
@@ -113,28 +147,62 @@ def plot_bcann(stretch, stress_true, stress_pred, stress_std_pred, kevins_versio
         if titles[i] is not None:
             axes[i].set_title(titles[i], y=1.05, usetex=False)
 
+    if terms:
+        labels = [x for In in range(1, 3) for x in
+                  [f"$(I_{In} - 3)$", f"exp$( (I_{In} - 3))$", f"$(I_{In} - 3)^2$", f"exp$( (I_{In} - 3)^2)$"]]
+        labels = labels + [x for dir in ["I_{4w}", "I_{4s}", "I_{4s_{I, II}}"] for x in
+                           [f"exp$({dir}) -  {dir}$", f"$({dir} - 1)^2$", f"exp$( ({dir} - 1)^2)$", ]]
+        legend_handles = [Patch(color=c) for c in cmaplist] + [scatterplot]
+        labels += ["data"]
+        leg = fig.legend(loc="lower center", ncols=4, handles=legend_handles, labels=labels,
+                         mode="expand", fontsize=40)
+        leg.get_frame().set_alpha(0)
 
 
-    rect_height = 0.349
-    x_offset = 0.005
-    engine = fig.get_layout_engine()
-    engine.set(rect=(0.005, 0.005, 0.99, 0.99), wspace=0.04)
-    for j in range(5):
-        rec = plt.Rectangle((0.2 * j-x_offset * (j < 3), rect_height), 0.2 + x_offset * (j == 2), 1 - rect_height, fill=False, lw=2)
+
+    if terms:
+        leg_height = 0.24
+        rect_height = 0.263
+        x_offset = 0.0
+        engine = fig.get_layout_engine()
+        engine.set(rect=(0.005, leg_height, 0.99, 0.995 - leg_height), wspace=0.04)
+        for j in range(5):
+            rec = plt.Rectangle((0.2 * j, leg_height + rect_height), 0.2, 1 - leg_height - rect_height, fill=False,
+                                lw=2)
+            rec.set_zorder(1000)
+            rec = fig.add_artist(rec)
+        for j in range(2):
+            rec = plt.Rectangle((0.4 * j, leg_height), 0.4, rect_height, fill=False, lw=2)
+            rec.set_zorder(1000)
+            rec = fig.add_artist(rec)
+        rec = plt.Rectangle((0.8, leg_height), 0.2, rect_height, fill=False, lw=2)
         rec.set_zorder(1000)
         rec = fig.add_artist(rec)
-    for j in range(2):
-        rec = plt.Rectangle((0.4 * j-x_offset, 0), 0.4 + x_offset * (j==1), rect_height, fill=False, lw=2)
+    else:
+        rect_height = 0.349
+        x_offset = 0.005
+        engine = fig.get_layout_engine()
+        engine.set(rect=(0.005, 0.005, 0.99, 0.99), wspace=0.04)
+        for j in range(5):
+            rec = plt.Rectangle((0.2 * j-x_offset * (j < 3), rect_height), 0.2 + x_offset * (j == 2), 1 - rect_height, fill=False, lw=2)
+            rec.set_zorder(1000)
+            rec = fig.add_artist(rec)
+        for j in range(2):
+            rec = plt.Rectangle((0.4 * j-x_offset, 0), 0.4 + x_offset * (j==1), rect_height, fill=False, lw=2)
+            rec.set_zorder(1000)
+            rec = fig.add_artist(rec)
+        rec = plt.Rectangle((0.8, 0), 0.2, rect_height, fill=False, lw=2)
         rec.set_zorder(1000)
         rec = fig.add_artist(rec)
-    rec = plt.Rectangle((0.8, 0), 0.2, rect_height, fill=False, lw=2)
-    rec.set_zorder(1000)
-    rec = fig.add_artist(rec)
     # Render and save plot
-    name = "kevin" if kevins_version else "jeremy"
+    name = "terms" if terms else "variance"
     plt.savefig(f"../Results/bcann_{name}.pdf",
                 transparent=False,
                 facecolor='white')
 
     plt.close()
+
+
+
+
 